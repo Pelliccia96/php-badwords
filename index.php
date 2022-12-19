621392588eccdf4ff1f0743cbee90163096b27e3
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-19 13:17:13 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+<body class="bg-dark">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="text-white">PHP Badwords</h1>
+        </div>
+    </div>
+</body>
+</html>
